62c3321ad78998753b9dc8d3d62d69be110e4946
Sam Gamble <hexadessa@gmail.com>
2018-10-18 17:25:22 +0100

    Update new client

--- oasislmf/api_client/client_v2.py
+++ oasislmf/api_client/client_v2.py
@@ -11,13 +11,14 @@ import time
 import logging
 
 class OasisAPIClient(object):
-    def __init__(self, api_url, api_ver,  logger=None):
+    def __init__(self, api_url, api_ver, timeout=5, logger=None):
         self._logger = logger or logging.getLogger()
         self.tkn_access  = None
         self.tkn_refresh = None
+        self.timeout = timeout
 
-        self.api_url     = api_url
-        self.api_vers    = api_ver
+        self.url_base    = api_url
+        self.url_vers    = api_ver
         self.api         = requests.Session()
         self.api.headers = {
             'authorization': '',
@@ -29,13 +30,16 @@ class OasisAPIClient(object):
         with io.open(os.path.abspath(file_path), 'rb') as f:
             m = MultipartEncoder(fields={
                 'file': (os.path.basename(file_path), f, content_type)})
-            return session.post(url, data=m,
+            return session.post(url, 
+                                data=m,
+                                timeout=self.timeout,
                                 headers={'Content-Type': m.content_type})
 
     def access(self, username, password):
-        url  = urljoin(self.api_url,'refresh_token/')
-        rsp  = self.api.post(url, json={"username": username,
-                                                  "password": password})
+        url  = urljoin(self.url_base,'refresh_token/')
+        rsp  = self.api.post(url, 
+                             timeout=self.timeout, 
+                             json={"username": username, "password": password})
         if rsp.status_code == status.ok:
             self.tkn_access  = rsp.json()['access_token']
             self.tkn_refresh = rsp.json()['refresh_token']
@@ -47,8 +51,8 @@ class OasisAPIClient(object):
 
     def refresh(self):
         self.api.headers['authorization'] = 'Bearer {}'.format(self.tkn_refresh)
-        url = urljoin(self.api_url,'access_token/')
-        rsp = self.api.post(url)
+        url = urljoin(self.url_base,'access_token/')
+        rsp = self.api.post(url, self.timeout)
 
         if rsp.status_code == status.ok:
             print(rsp.json())
@@ -63,7 +67,7 @@ class OasisAPIClient(object):
 
 
     #@oasis_log
-    def health_check(self, poll_attempts=1, retry_delay=5):
+    def health_check(self, retry_delay=5, retrys=1):
         """
         Checks the health of the server.
 
@@ -75,52 +79,46 @@ class OasisAPIClient(object):
 
         :return: True If the server is healthy, otherwise False
         """
-        url = urljoin(self.api_url, 'helthcheck/')
-        rsp = self.api.get(url)
+        try: 
+            url = urljoin(self.url_base, 'helthcheck/')
+            rsp = self.api.get(url, timeout=1)
+            rsp.raise_for_status()
+            return True 
+        except Exception as e:
+            if retrys > 0:     
+                print('Failed - Retry')
+                time.sleep(retry_delay)
+                self.health_check(retry_delay=retry_delay, retrys=retrys-1)
+            
+            self._logger.error('Healthcheck failed: {}.'.format(str(e)))
+        return False
 
-        for attempt in range(poll_attempts):
-            try:
-                if attempt > 0:
-                    rsp = self.api.get(url)
-                if rsp.ok:
-                    return True
-            except RequestException:
-                pass
-
-            time.sleep(retry_delay)
-        else:
-            self._logger.error(
-                'Could not connect to the api server after {} attempts. Check it is running and try again later.'.format(
-                    poll_attempts,
-                )
-            )
-            return False
 
     def create_model(self, supplier_id, model_id, version_id, retrys=1):
         try: 
             data = {"supplier_id": supplier_id, "model_id": model_id, "version_id":version_id} 
-            url  = urljoin(self.api_url, self.api_vers, 'models/')
+            url  = urljoin(self.url_base, self.url_vers, 'models/')
             rsp  = self.api.post(url, json=data)
             rsp.raise_for_status()
-            return rsp
+            # Req complete  
+            return rsp.json()
         except (HTTPError, Timeout, ConnectionError) as e:
             if retrys > 0:
                 if rsp.status_code == status.UNAUTHORIZED:
                     self.refresh()
                     self.add_model(supplier, model, version, retrys=retrys-1)
-            else:
-                # Log error & raise execption 
-                pass
+        # req failed
         return rsp
 
 
-    def create_portfolio(self, portfolio_name, src_acc_fp, src_loc_fp, retrys=1):
+    def create_portfolio(self, portfolio_name, retrys=1):
         try: 
             data = {"name": portfolio_name}
-            url  = urljoin(self.api_url, self.api_vers, 'portfolios/')
+            url  = urljoin(self.url_base, self.url_vers, 'portfolios/')
             rsp  = self.api.post(url, json=data)
             rsp.raise_for_status()
-            return rsp
+            # Req complete  
+            return rsp.json()
         except (HTTPError, Timeout, ConnectionError) as e:
             if retrys > 0:
                 # Refresh token and resend request
@@ -129,35 +127,56 @@ class OasisAPIClient(object):
                     self.create_analyses()
 
                 # Bad Request or model alreadys exsists
-                if rsp.status_code == status.:
+                if rsp.status_code == status.BAD:
+                    return rsp
+        # req failed
+        return rsp
+   
 
-            else:
-                # Log error & raise execption 
-                return rsp
-    
-
-
-'''
-    def create_analyses(self, analyses_name, model_id, fp_src_acc, fp_src_loc, retrys=1):
+    def create_analyses(self, analyses_name, model_id, portfolio_id, retrys=1):
         try: 
-            data = {"name": analyses_name, "model": str(model_id)}
-            url  = 
-            rsp  = 
+            data = {"name": analyses_name,
+                    "portfolio": model_id,
+                    "model": portfolio_id}
+            url  = urljoin(self.url_base, self.url_vers, 'analyses/')
+            rsp  = self.api.post(url, json=data)
             rsp.raise_for_status()
-            return rsp
-         except HTTPError, Timeout, ConnectionError as e:
+            # Req complete
+            return rsp.json()
+        except (HTTPError, Timeout, ConnectionError) as e:
             if retrys > 0:
+                # Refresh token and resend request
                 if rsp.status_code == status.UNAUTHORIZED:
                     self.refresh()
-                self.create_analyses()
-            else:
-                # Log error & raise execption 
-                return False
-'''
+                    self.create_analyses(analyses_name, model_id, portfolio_id, retrys=retrys-1)
+        #req failed
+        return rsp
 
 
-'''
+ #   def upload_exposure(self, portfolio_dict, fp_location, fp_account, fp_ri_info=None, fp_ri_scope=None):
+ #       try:
+ #           
+
+ #       try: 
+ #           url  = 
+ #           rsp  = 
+ #           rsp.raise_for_status()
+ #           return rsp
+ #       except (HTTPError, Timeout, ConnectionError) as e:
+ #           if retrys > 0:
+ #               if rsp.status_code == status.UNAUTHORIZED:
+ #                   self.refresh()
+ #               self.create_analyses()
+
+ #       #req failed
+ #       return False
+
+        
+
+
+
 ## -------------------------------------------------------------------------- #
+'''
 Methods to override
 
 class OasisAPIClient(object):
