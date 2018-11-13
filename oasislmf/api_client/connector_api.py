ef7b2d9fbdfce85c272e1ca47f1cb2618bd00850
sam gamble <hexadessa@gmail.com>
2018-11-13 13:51:45 +0000

    Update api connector

--- oasislmf/api_client/connector_api.py
+++ oasislmf/api_client/connector_api.py
@@ -15,29 +15,29 @@ import time
 import logging
 
 class connector(object):
-    def __init__(self, api_url, api_ver, username, password, logger=None):
+    def __init__(self, api_url, api_ver, username, password, timeout=1, logger=None):
         self._logger = logger or logging.getLogger()
         self.tkn_access  = None
         self.tkn_refresh = None
 
         self.url_base    = api_url
         self.url_vers    = api_ver
-        #self.api         = requests.Session()
+        self.timeout     = timeout
         self.api         = self._session_retry()
         self.api.headers = {
             'authorization': '',
             'accept': 'application/json',
             'content-type': 'application/json',
         }
-        access_granted = self._get_access_token(username, password) 
-        if access_granted is not True:
 
-        except Exception as e:
-            print('API Authentication error')
-            print(e)
+        # Check connectivity 
+        self.health_check()
+        # Check authentication  
+        self._get_access_token(username, password) 
 
 
     def _session_retry(
+        self,
         retries=3,
         backoff_factor=0.3,
         status_forcelist=(500, 502, 504),
@@ -48,6 +48,7 @@ class connector(object):
             total=retries,
             read=retries,
             connect=retries,
+            status=retries,
             backoff_factor=backoff_factor,
             status_forcelist=status_forcelist,
         )
@@ -58,29 +59,35 @@ class connector(object):
 
     def _get_access_token(self, username, password):
         url  = urljoin(self.url_base,'refresh_token/')
-        rsp  = self.api.post(url, json={"username": username, "password": password})
-        if rsp.status_code == status.ok:
-            self.tkn_access  = rsp.json()['access_token']
-            self.tkn_refresh = rsp.json()['refresh_token']
+        r = self.api.post(url, json={"username": username, "password": password})
+
+        if r.status_code == status.ok:
+            self.tkn_access  = r.json()['access_token']
+            self.tkn_refresh = r.json()['refresh_token']
             self.api.headers['authorization'] = 'Bearer {}'.format(self.tkn_access)
-            return True        
+        elif r.status_code == status.UNAUTHORIZED:    
+            print('API Login failed:')
+            print(r.text)
+            #Raise Oasis Error
         else:
-            # Log error & raise execption 
-            return rsp.text
+            print('API error on fetching access token:')
+            print(r.text)
+            #Raise Oasis Error
+        return r
 
     def _refresh_token(self):
         self.api.headers['authorization'] = 'Bearer {}'.format(self.tkn_refresh)
         url = urljoin(self.url_base,'access_token/')
-        rsp = self.api.post(url, self.timeout)
+        r = self.api.post(url, self.timeout)
 
-        if rsp.status_code == status.ok:
-            print(rsp.json())
+        if r.status_code == status.ok:
             self.tkn_access  = rsp.json()['access_token']
             self.api.headers['authorization'] = 'Bearer {}'.format(self.tkn_access)
-            return True
         else:
-            # Log error & raise execption 
-            return rsp.text
+            print('API error when refreshing token:')
+            print(r.text)
+            #Raise Oasis Error
+        return r
 
 
 
@@ -97,70 +104,72 @@ class connector(object):
         pass
 
     #@oasis_log
-    def health_check(self, retry_delay=5, retrys=1):
+    def health_check(self):
         """
         Checks the health of the server.
 
-        :param poll_attempts: The maximum number of checks to make
-        :type poll_attempts: int
-
-        :param retry_delay: The amount of time to wait between retry attempts
-        :type retry_delay: int
-
-        :return: True If the server is healthy, otherwise False
         """
         try: 
             url = urljoin(self.url_base, 'helthcheck/')
-            rsp = self.api.get(url, timeout=1)
-            rsp.raise_for_status()
+            r = self.api.get(url, timeout=self.timeout)
             return True 
         except Exception as e:
-            if retrys > 0:     
-                print('Failed - Retry')
-                time.sleep(retry_delay)
-                self.health_check(retry_delay=retry_delay, retrys=retrys-1)
-            
-            self._logger.error('Healthcheck failed: {}.'.format(str(e)))
-        return False
+            print('Connection check Failed:')
+            print(e) 
+            #self._logger.error('Healthcheck failed: {}.'.format(str(e)))
 
 
-    def get(self, url, **kwargs):
+    def get(self, url_endpoint, url_base=None,**kwargs):
         try: 
-            r = self.api.get(url, **kwargs)
+            if not url_base:
+                url_base = self.url_base
+            r = self.api.get(urljoin(url_endpoint, url_base), timeout=self.timeout, **kwargs)
         except Exception as e:
            if r.status_code == status.UNAUTHORIZED:
                self._refresh_token()
+        r.raise_for_status()
         return r
 
-    def post(self, url, **kwargs):
+    def post(self, url_endpoint, url_base=None, **kwargs):
         try: 
-            r = self.api.post(url, **kwargs)
+            if not url_base:
+                url_base = self.url_base
+            r = self.api.post(urljoin(url_endpoint, url_base), timeout=self.timeout, **kwargs)
         except Exception as e:
            if r.status_code == status.UNAUTHORIZED:
                self._refresh_token()
+        r.raise_for_status()
         return r
 
-    def delete(self, url, **kwargs):
+    def delete(self, url_endpoint, url_base=None, **kwargs):
         try: 
-            r = self.api.delete(url, **kwargs)
+            if not url_base:
+                url_base = self.url_base
+            r = self.api.delete(urljoin(url_endpoint, url_base), timeout=self.timeout, **kwargs)
         except Exception as e:
            if r.status_code == status.UNAUTHORIZED:
                self._refresh_token()
+        r.raise_for_status()
         return r
 
-    def put(self, url, **kwargs):
+    def put(self, url_endpoint, url_base=None, **kwargs):
         try: 
-            r = self.api.put(url, **kwargs)
+            if not url_base:
+                url_base = self.url_base
+            r = self.api.put(urljoin(url_endpoint, url_base), timeout=self.timeout, **kwargs)
         except Exception as e:
            if r.status_code == status.UNAUTHORIZED:
                self._refresh_token()
+        r.raise_for_status()
         return r
 
-    def patch(self, url, **kwargs):
+    def patch(self, url_endpoint, url_base=None, **kwargs):
         try: 
-            r = self.api.patch(url, **kwargs)
+            if not url_base:
+                url_base = self.url_base
+            r = self.api.patch(urljoin(url_endpoint, url_base), timeout=self.timeout, **kwargs)
         except Exception as e:
            if r.status_code == status.UNAUTHORIZED:
                self._refresh_token()
+        r.raise_for_status()
         return r
-
